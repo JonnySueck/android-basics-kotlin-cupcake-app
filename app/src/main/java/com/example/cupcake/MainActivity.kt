088b7b45fd7e29fccc3b2cb2347981af1bd0241e
Jonathan Sueck <jonathansueck@gmail.com>
2021-12-14 22:20:13 -0500

    added in a onCreateView to the mainActivity file to create a reference of the NavController, and update the title based on the fragments destination label. Connected destinations via the nav graph and added findNavController().navigate(R.id.fragment_id) into each fragment to navigate when the buttons are clicked.

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,25 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    // get an instance of NavController from NavHostFragment
+    val navHostFragment = supportFragmentManager
+        .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    val navController = navHostFragment.navController
+
+    // show a title in the app bar based on the destination's label & display
+        // the Up button when you're not at a top level destination
+    setupActionBarWithNavController(navController)
+    }
+}
